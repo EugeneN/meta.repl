841d17bb367689f71d788ecfdbfc2fc9d425d826
Eugene <eugene.naumenko@gmail.com>
2015-12-11 20:43:25 +0200

    cleanup

--- src/UI/HTML/Main.js
+++
@@ -1,9 +0,0 @@
-// module UI.HTML.Main
-
-exports.injectBody = function (html) {return function() {document.body.innerHTML = html; return {}; } }
-exports.appendToBody = function (node) {return function() {document.body.appendChild(node); return {}; } }
-exports.toString = function (a) {
-  console.log(a)
-  return a.toString() }
-
-exports.vNode2vTree = function(vnode) { return vnode }
